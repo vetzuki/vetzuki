c0acc2f07b3390cabbace95e65b0f438e96affb8
Bruce <iambruce@me.com>
2019-08-24 08:23:58 -0700

    Cleanup
    Organized the host data into a `vetzuki-host` folder and added a `src/tools` folder to hold things like the `POST` reflector

--- src/tools/main.go
+++ src/tools/main.go
@@ -5,6 +5,13 @@ import "net/http"
 import "encoding/json"
 import "os"
 
+/*
+Sinple HTTP listener to print POST content to a screen
+given any route.
+
+The route is configured with the env var API_ROUTE. If
+it isn't given, the route is /
+*/
 func handler(w http.ResponseWriter, r *http.Request) {
 	var v interface{}
 	d := json.NewDecoder(r.Body)
